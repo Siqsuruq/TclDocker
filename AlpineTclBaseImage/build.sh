1389a01eae2154351bbd6c19778cf4c383e04e1b
Maksym Zinchenko <siqsuruq@gmail.com>
2026-02-07 15:58:33 -0100

    Malloc
    system malloc patched

--- AlpineTclBaseImage/build.sh
+++ AlpineTclBaseImage/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build --pull --no-cache -f Dockerfile -t tcl9-alpine .
+docker build --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") --pull --no-cache -f DockerfileTcl9 -t tcl9-alpine .
+docker build --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") --pull --no-cache -f DockerfileTcl8 -t tcl-alpine .
