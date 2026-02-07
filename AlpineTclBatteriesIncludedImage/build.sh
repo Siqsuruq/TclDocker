e30d22fc8f75681fdc5642437a72d163cf189e89
Maksym Zinchenko <siqsuruq@gmail.com>
2026-02-07 18:27:50 -0100

    Build. sh update
    Build. sh update

--- AlpineTclBatteriesIncludedImage/build.sh
+++ AlpineTclBatteriesIncludedImage/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker build --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") --pull --no-cache -f DockerfileTcl9 -t tcl9bi-alpine .
+docker build --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") --pull --no-cache -f DockerfileSysMallocTcl9BI -t tcl9bi-alpine-malloc .
